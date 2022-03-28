3e6da8625b7ec13c87043e8cb4e53ee54d89fae3
TaylorMade <warriorotk18@gmail.com>
2022-03-27 23:53:08 -0600

    Called a variable as an array element and played around with it

--- arraypractice.js
+++ arraypractice.js
@@ -1,9 +1,15 @@
 // Forgotten that the brackets for arrays were called 'array literals'
 
+const Leon = 'Leon is the absolute sickest RE character EVER!';
+
 const superStars = [ 
-    Mario,
-    Link,
-    DonkeyKong,
+    'Mario',
+    'Link',
+    'DonkeyKong',
     Leon,
-    Cloud
+    'Cloud'
 ];
+
+console.log(superStars[3]);
+
+//Just did a little experimentation with an array, having all the other array elements as strings, I called index 3 which was a variable I defined. Pretty cool
