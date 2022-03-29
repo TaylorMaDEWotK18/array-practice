fb2b22e994cf1710d9fcfb70587c35f3b0ac4cf8
TaylorMade <warriorotk18@gmail.com>
2022-03-28 19:53:35 -0600

    Created array with numbers and added total up with for loop!

--- arraypractice.js
+++ arraypractice.js
@@ -13,3 +13,13 @@ const superStars = [
 console.log(superStars[3]);
 
 //Just did a little experimentation with an array, having all the other array elements as strings, I called index 3 which was a variable I defined. Pretty cool
+
+//Below making an array full of numbers and finding the total in a for loop
+
+const howMany = [ 12, 2, 9 ];
+let sum = 0;
+for ( let i = 0; i < howMany.length; i++) {
+    sum += howMany[i];
+}
+
+console.log(sum);
