79488427b8b2b1cb96a715b77fe7da172aa1e31e
TaylorMade <warriorotk18@gmail.com>
2022-03-28 20:03:08 -0600

    set up variables for q #3

--- arraypractice.js
+++ arraypractice.js
@@ -26,7 +26,10 @@ console.log(sum);
 
 //#3) Get the sum of two arrays…actually the sum of all their elements.
 
-// let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
-// let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
+let set1 = [17, 34, 2, 9, 84];
+let set2 = [18, 92, 4, 56, 3];
+let sum1 = 0;
+
+
 // // Example output: 
 // // 276 + 351 = 627
