51c20272a35e4849aa696244bf1faca5c748fa96
TaylorMade <warriorotk18@gmail.com>
2022-03-28 19:58:34 -0600

    Numbered up list of q's and beginning q #3

--- arraypractice.js
+++ arraypractice.js
@@ -1,4 +1,4 @@
-// Forgotten that the brackets for arrays were called 'array literals'
+//#1) Forgotten that the brackets for arrays were called 'array literals'. Example of a simple array
 
 const Leon = 'Leon is the absolute sickest RE character EVER!';
 
@@ -14,7 +14,7 @@ console.log(superStars[3]);
 
 //Just did a little experimentation with an array, having all the other array elements as strings, I called index 3 which was a variable I defined. Pretty cool
 
-//Below making an array full of numbers and finding the total in a for loop
+//#2) - Below making an array full of numbers and finding the total in a for loop
 
 const howMany = [ 12, 2, 9 ];
 let sum = 0;
@@ -23,3 +23,10 @@ for ( let i = 0; i < howMany.length; i++) {
 }
 
 console.log(sum);
+
+//#3) Get the sum of two arrays…actually the sum of all their elements.
+
+// let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
+// let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
+// // Example output: 
+// // 276 + 351 = 627
