e299112c94f6540d7de9a3c2b8f6278e5a104d39
TaylorMade <warriorotk18@gmail.com>
2022-03-30 23:44:35 -0600

    Built the function to store the list items in the playlist

--- playlist.js
+++ playlist.js
@@ -1,1 +1,8 @@
-let songsBaby = [ 'Get Down', 'Beat It', 'Rocket Man', 'Welcome to the Jungle']
+const songsBaby = [ 'Get Down', 'Beat It', 'Rocket Man', 'Welcome to the Jungle'];
+
+//Create a function to take each of the elements in the array and place them in the item list. The function will need to accept the array as an arguement to create the list. Inside the code block we'll create the items variable to store each list item for the playlist. With this items variable, I can build and tore the entire string before returning it and displaying it on the page.
+
+function createListItem(arr) {
+    let items = '';
+}
+
