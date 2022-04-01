e97de61b638355790d205aa7cd105d9c819f0ec6
TaylorMade <warriorotk18@gmail.com>
2022-04-01 00:28:03 -0600

    Quick little for loop challenge entered

--- playlist.js
+++ playlist.js
@@ -1,4 +1,4 @@
-const songsBaby = [ 'Get Down', 'Beat It', 'Rocket Man', 'Welcome to the Jungle'];
+const songsBaby = [ 'Get Down', 'Beat It', 'Rocket Man', 'Welcome to the Jungle', 'PTY'];
 
 //#1) Create a function to take each of the elements in the array and place them in the item list. The function will need to accept the array as an arguement to create the list. Inside the code block we'll create the items variable to store each list item for the playlist. With this items variable, I can build and tore the entire string before returning it and displaying it on the page.
 
@@ -20,3 +20,8 @@ document.querySelector('main').innerHTML = `
     </ol>
     `;
     
+//This was just a quick little for loop challenge to loop through the array    
+const temperatures = [100, 90, 99, 80, 70, 65, 30, 10];
+for ( let i = 0; i < temperatures.length; i++ ) {
+    console.log(temperatures[i]);
+}
