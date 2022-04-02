bf67056333bc624a43927950c2be0f7b9fd3dc2e
TaylorMade <warriorotk18@gmail.com>
2022-04-01 23:58:55 -0600

    added space to help seperate elements

--- arraypractice.js
+++ arraypractice.js
@@ -54,4 +54,4 @@ for (let p = 0; p < 3; p++) {
 // Now to practice the join method in an array
 
 const bestAnime = [ 'Hunter D', 'DBZ', 'Akira', 'Guyver', 'Demon Slayer']
-console.log(bestAnime.join(','));
+console.log(bestAnime.join(', '));
