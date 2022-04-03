258fdf0891d2d56f49703c5caa2b18d632708149
TaylorMade <warriorotk18@gmail.com>
2022-04-03 00:18:57 -0600

    completed code, merging branch

--- js/itemlist.js
+++ js/itemlist.js
@@ -1,10 +1,12 @@
 const retroSystems = ['N64', 'NES', 'SNES', 'PlayStation', 'Sega', 'GameBoy', 'Game Gear']
 const search = prompt('What system are you looking for?');
-let message
+let message;
 
 // So the goal here is to search whether the system is in stock and have the page display the answer and what we searched for. We'll use the 'includes' method and a conditional statement
 if (retroSystems.includes(search)) {
-    message = `Yes! Get ready to game cuz we have the ${seach} in stock!`;
+    message = `Yes! Get ready to game cuz we have the <strong>${search}</strong> in stock!`;
 } else {
     message = `No I'm sorry, we're fresh out of ${search}`;
 }
+
+document.querySelector('main').innerHTML = `<p>${message}</p>`;
