375332d08e90937d6bf4d80b61cf2b58c7acddca
TaylorMade <warriorotk18@gmail.com>
2022-04-03 00:11:38 -0600

    corrected if statement

--- js/itemlist.js
+++ js/itemlist.js
@@ -3,7 +3,7 @@ const search = prompt('What system are you looking for?');
 let message
 
 // So the goal here is to search whether the system is in stock and have the page display the answer and what we searched for. We'll use the 'includes' method and a conditional statement
-if (retroSystems.includes === search) {
+if (retroSystems.includes(search)) {
     message = `Yes! Get ready to game cuz we have the ${seach} in stock!`;
 } else {
     message = `No I'm sorry, we're fresh out of ${search}`;
