4d2c034fdf34d70b13ec61ec611edbcc2667a1ce
TaylorMade <warriorotk18@gmail.com>
2022-04-03 23:39:45 -0600

    Built new array to practice

--- js/itemlist.js
+++ js/itemlist.js
@@ -1,12 +1,25 @@
-const retroSystems = ['N64', 'NES', 'SNES', 'PlayStation', 'Sega', 'GameBoy', 'Game Gear']
-const search = prompt('What system are you looking for?');
-let message;
+// const retroSystems = ['N64', 'NES', 'SNES', 'PlayStation', 'Sega', 'GameBoy', 'Game Gear']
+// const search = prompt('What system are you looking for?');
+// //let message;
 
-// So the goal here is to search whether the system is in stock and have the page display the answer and what we searched for. We'll use the 'includes' method and a conditional statement
-if (retroSystems.includes(search)) {
-    message = `Yes! Get ready to game cuz we have the <strong>${search}</strong> in stock!`;
+// // So the goal here is to search whether the system is in stock and have the page display the answer and what we searched for. We'll use the 'includes' method and a conditional statement
+// if (retroSystems.includes(search)) {
+//     message = `Yes! Get ready to game cuz we have the <strong>${search}</strong> in stock!`;
+// } else {
+//     message = `No I'm sorry, we're fresh out of ${search}`;
+// }
+
+// document.querySelector('main').innerHTML = `<p>${message}</p>`;
+
+
+const kingBooks = ['Carrie', 'Salems Lot', 'Cujo', 'Pet Semetary', 'The Shining', 'The Stand']
+let message;
+let searcch = prompt(`What's your favorite Stephen King book you've read?`);
+
+if (kingBooks.includes(searcch)) {
+    message = `Sick! ${searcch} is AMAZING!! Great choice!`;
 } else {
-    message = `No I'm sorry, we're fresh out of ${search}`;
+    message = `Oh dang! Never read ${searcch}, gonna have to give that one a shot.`;
 }
 
-document.querySelector('main').innerHTML = `<p>${message}</p>`;
+document.querySelector('main').innerHTML = message;
