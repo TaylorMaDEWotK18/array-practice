7c62ddbdd91a92564e75b1da515b5195a706bfbb
TaylorMade <warriorotk18@gmail.com>
2022-04-06 00:05:21 -0600

    created forLoop to display all the games

--- js/multiarray.js
+++ js/multiarray.js
@@ -3,3 +3,11 @@ const games = [
     ['Sonic', 'Streets of Rage', 'Vector Man'],
     ['Super Smash', ' Mario 64', 'Shadows of the Empire']
 ]
+
+const snesGames = games[0];
+const segaGames = games[1];
+const n64Games = games[2];
+
+for (let x = 0; x < games.length; x++) {
+    console.log(games[x]);
+}
