ba44e1709ec3fa0f29e476137a25e3d72cc844ba
TaylorMade <warriorotk18@gmail.com>
2022-04-07 00:01:14 -0600

    created function for loop in array

--- js/playlist2.js
+++ js/playlist2.js
@@ -5,3 +5,10 @@ const retroList = [
     ['Final Fantasy 9', 'PS1', '2000'],
     ['Metal Gear Solid 3', 'PS2', '2004']
 ]
+
+function createRetroList( arr ) {
+    games = ``;
+    for ( let x = 0; x < arr.length; x++) {
+        games += `<li>${arr[x][0]} was for the ${arr[x][1]} developed in the year ${arr[x][2]}</li>`
+    }
+}
