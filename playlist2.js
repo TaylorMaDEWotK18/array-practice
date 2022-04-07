8115d54edd9a504ffe7057d8551af24966b72f3a
TaylorMade <warriorotk18@gmail.com>
2022-04-06 23:52:54 -0600

    built retro array for playlist

---
+++ playlist2.js
@@ -0,0 +1,7 @@
+const retroList = [
+    ['Zelda: Ocarina of Time', 'N64', '1998'],
+    ['Resident Evil 2', 'PS1', '1998'],
+    ['Super Mario World', 'SNES', '1990'],
+    ['Final Fantasy 9', 'PS1', '2000'],
+    ['Metal Gear Solid 3', 'PS2', '2004']
+]
